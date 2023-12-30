9de698db2bb29970bb4b34bd4b22465bc780267f
Dario <dariomadau@hotmail.com>
2023-12-31 00:21:56 +0100

    added succes / fail screen

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Show input changes on form
-document.addEventListener("DOMContentLoaded", function () {
-  const telegramUsername = document.getElementById("telegramUsername");
-  const minPrice = document.getElementById("minPrice");
-  const maxPrice = document.getElementById("maxPrice");
-  const minSize = document.getElementById("minSize");
-  const maxSize = document.getElementById("maxSize");
-  const priceRange = document.getElementById("priceRange");
-  const sizeRange = document.getElementById("sizeRange");
+document.addEventListener('DOMContentLoaded', function () {
+  const telegramUsername = document.getElementById('telegramUsername');
+  const minPrice = document.getElementById('minPrice');
+  const maxPrice = document.getElementById('maxPrice');
+  const minSize = document.getElementById('minSize');
+  const maxSize = document.getElementById('maxSize');
+  const priceRange = document.getElementById('priceRange');
+  const sizeRange = document.getElementById('sizeRange');
 
   function updatePriceRange() {
     priceRange.textContent = `€${minPrice.value} - €${maxPrice.value}`;
@@ -18,16 +18,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Function to ensure '@' at the start of the Telegram username
   function ensureAtSymbol() {
-    if (telegramUsername.value && !telegramUsername.value.startsWith("@")) {
-      telegramUsername.value = "@" + telegramUsername.value;
+    if (telegramUsername.value && !telegramUsername.value.startsWith('@')) {
+      telegramUsername.value = '@' + telegramUsername.value;
     }
   }
 
-  telegramUsername.addEventListener("input", ensureAtSymbol);
-  minPrice.addEventListener("input", updatePriceRange);
-  maxPrice.addEventListener("input", updatePriceRange);
-  minSize.addEventListener("input", updateSizeRange);
-  maxSize.addEventListener("input", updateSizeRange);
+  telegramUsername.addEventListener('input', ensureAtSymbol);
+  minPrice.addEventListener('input', updatePriceRange);
+  maxPrice.addEventListener('input', updatePriceRange);
+  minSize.addEventListener('input', updateSizeRange);
+  maxSize.addEventListener('input', updateSizeRange);
 
   updatePriceRange();
   updateSizeRange();
